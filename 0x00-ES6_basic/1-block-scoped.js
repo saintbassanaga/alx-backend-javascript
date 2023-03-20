796974bb109e9a3a21e2aa754a732eb555711218
saintbassanaga <saintbassanaga01@gmail.com>
2023-03-20 08:37:12 -0700

    Update file

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
-export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+export default function taskBlock (trueOrFalse) {
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    var task = false;
-    var task2 = true;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];

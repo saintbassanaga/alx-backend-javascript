2a8b2a258bf0eed798bd60d1cb190c700fd21f4c
saintbassanaga <saintbassanaga01@gmail.com>
2023-03-23 00:38:06 -0700

    Update

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,10 @@
-/* eslint-disable no-console */
-// eslint-disable-next-line import/extensions
-import { uploadPhoto, createUser } from './util.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  uploadPhoto().then((response) => {
-    createUser().then((user) => {
-      console.log(`${response.body} ${user.firstName} ${user.lastName}`);
-    }).catch(() => {
-      console.log('Signup system offline');
-    });
-  });
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
